dbafe00420ee950af26af7372baf15cefc0a13c9
jhakkari <juuso.hakkari@helsinki.fi>
2023-04-25 20:02:24 +0300

    Fix invoke task

--- tasks.py
+++ tasks.py
@@ -5,7 +5,7 @@ def start(ctx):
     ctx.run("python3 src/index.py", pty=True)
 
 @task
-def built(ctx):
+def build(ctx):
     ctx.run("python3 src/initialize_db.py", pty=True)
 
 @task
@@ -18,6 +18,7 @@ def coverage(ctx):
 
 @task
 def coverage_report(ctx):
+    ctx.run("coverage run --branch -m pytest src", pty=True)
     ctx.run("coverage html", pty=True)
 
 @task
